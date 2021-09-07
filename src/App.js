89e37f0b6cf604155c7e6023643c04924e873628
OwenZou-TheBot <79731673+OwenZou-TheBot@users.noreply.github.com>
2021-09-06 20:52:51 -0400

    't'

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AddingNumbers from './components/AddingNumbers'
+
+function App() {
+  return (
+    <div className='App'>
+      <AddingNumbers />
+    </div>
+  );
+}
+
+export default App;
