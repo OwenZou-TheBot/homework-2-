dc803cc3b44704e4aa89b98f832e57594dcb2660
OwenZou-TheBot <79731673+OwenZou-TheBot@users.noreply.github.com>
2021-09-08 20:35:50 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import AddingNumbers from './components/AddingNumbers'
+import Todolist from './components/AddingNumbers'
 
 function App() {
   return (
     <div className='App'>
-      <AddingNumbers />
+      <Todolist />
     </div>
   );
 }
